01efa4a34239313acedc40a79f51fd6b587db286
strange-wonders <snorris@ppark-dhcp-192-168-50-69.talari.tnet>
2015-08-10 13:54:38 -0400

    updating igor

--- igorifier.rb
+++ igorifier.rb
@@ -4,12 +4,19 @@ print "Thtring pleathe, mathter:"
 
 user_input =  gets.chomp
 
+downflag=false
+upflag=false
 if user_input.include? "s"
 	user_input.gsub!(/s/, "th")
+	downflag = true
 end
 if user_input.include? "S"
-			user_input.gsub!(/S/, "Th")
+	user_input.gsub!(/S/, "Th")
+	upflag = true		
 end
 
-puts "Thank you, mathter. Your thtring ith: #{user_input}"
-
+if downflag = true || upflag=true
+	puts "Thank you, mathter. Your thtring ith: #{user_input}"
+else
+	puts "There ith nothing to change, mathter."
+end
